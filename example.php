1e18e0af5c1590fb84c8d0fcc60640f5af7c5e6a
WebBreacher <WebBreacher@users.noreply.github.com>
2019-02-08 10:57:26 -0500

    changing the DB

--- example.php
+++ example.php
@@ -16,7 +16,8 @@ require_once 'IP2Location.php';
 /*
 	Default file I/O lookup
 */
-$db = new \IP2Location\Database('./databases/IP2LOCATION-LITE-DB11.BIN', \IP2Location\Database::FILE_IO);
+#$db = new \IP2Location\Database('./databases/IP2LOCATION-LITE-DB11.BIN', \IP2Location\Database::FILE_IO);
+$db = new \IP2Location\Database('./databases/IP2PROXY-LITE-PX4.BIN', \IP2Location\Database::FILE_IO);
 $ip=$_SERVER['REMOTE_ADDR'];
 $records = $db->lookup($ip, \IP2Location\Database::ALL);
 
