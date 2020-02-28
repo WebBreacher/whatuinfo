6a07bdca42d0e5fb8de0769262f072b92465d901
WebBreacher <WebBreacher@users.noreply.github.com>
2020-02-27 22:52:44 -0500

    updating dbs

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <?php
     require_once 'IP2Location.php';
-    
+
     $ip=$_SERVER['REMOTE_ADDR'];
     $agent=htmlspecialchars($_SERVER['HTTP_USER_AGENT']);
     $refer=htmlspecialchars($_SERVER['HTTP_REFERER']);
 
     # IP2Location Stuff
-    $db = new \IP2Location\Database('./databases/IP2PROXY-LITE-PX4.BIN', \IP2Location\Database::FILE_IO);
+    $db = new \IP2Location\Database('./databases/IP2PROXY-LITE-PX8.BIN', \IP2Location\Database::FILE_IO);
     $ip=$_SERVER['REMOTE_ADDR'];
     $records = $db->lookup($ip, \IP2Location\Database::ALL);
     $vpn=htmlspecialchars($records['countryCode']);
+    $proxyType=htmlspecialchars($records['proxy_type']);
+    $isp=htmlspecialchars($records['isp']);
+    $usageType=htmlspecialchars($records['usage_type']);
 
     $db = new \IP2Location\Database('./databases/IP2LOCATION-LITE-DB11.BIN', \IP2Location\Database::FILE_IO);
     $records = $db->lookup($ip, \IP2Location\Database::ALL);
@@ -30,7 +33,7 @@
     print "    <link rel='stylesheet' href='w3.css'>\n";
     print "    <link rel='stylesheet' href='w3-colors-metro.css'>\n";
     print "    <style>\n";
-    print "        p.header {font-weight:bold;}\n"; 
+    print "        p.header {font-weight:bold;}\n";
     print "        div.center {margin: auto; width: 80%; padding: 10px;}\n";
     print "        td {vertical-align: middle; padding: 15px;)\n";
     print "        td.mono {font-family: monospace; font-size:18px!important;}\n";
@@ -38,7 +41,7 @@
     print "</head>\n";
 
     print "<body>\n";
-    
+
     print "<div class='w3-container'>\n";
     print "    <div class='w3-panel w3-card-2 w3-metro-dark-blue'>\n";
     print "        <h2>This is how your browser appears to other sites.</h2>\n";
@@ -48,7 +51,7 @@
 
     print "  <div class='center w3-panel w3-card-2 w3-round-xlarge'>\n";
     print "     <table class='w3-table-all'>\n";
-    print "     <h4>General Data:</h4>\n"; 
+    print "     <h4>General Data:</h4>\n";
     print "         <thead><tr class='w3-light-grey'><th>Item</th><th>Value</th></tr></thead>\n";
     print "         <tr class='w3-hover-blue'><td class='header'>Your IP Address:</td><td class='mono'>$ip</td></tr>\n";
     print "         <tr class='w3-hover-black'><td class='header'>Your User-Agent:</td><td class='mono'>$agent</td></tr>\n";
@@ -57,15 +60,18 @@
     print "     </br>\n";
     print "  </div>\n";
     print "  </br>\n";
-    
+
     print "  <div class='center w3-panel w3-card-2 w3-round-xlarge'>\n";
-    print "     <h4>IP2Location Data:</h4> <!-- https://github.com/chrislim2888/IP2Location-PHP-Module -->\n"; 
+    print "     <h4>IP2Location Data:</h4> <!-- https://github.com/chrislim2888/IP2Location-PHP-Module -->\n";
     print "     <table class='w3-table-all'>\n";
     print "         <thead><tr class='w3-light-grey'><th>Item</th><th>IP2Location Value</th></tr></thead>\n";
     print "         <tr class='w3-hover-blue'><td class='header'>Country Name (Code):</td><td class='mono'>$countryname ( $countrycode )</td></tr>\n";
     print "         <tr class='w3-hover-black'><td class='header'>City, Region, Zip Code:</td><td class='mono'>$city, $region    $zipcode</td></tr>\n";
     print "         <tr class='w3-hover-blue'><td class='header'>Latitude, Longitude:</td><td class='mono'>$latlon</td></tr>\n";
     print "         <tr class='w3-hover-black'><td class='header'>VPN Status:</td><td class='mono'>$vpn</td></tr>\n";
+    print "         <tr class='w3-hover-black'><td class='header'>Proxy Type:</td><td class='mono'>$proxyType</td></tr>\n";
+    print "         <tr class='w3-hover-black'><td class='header'>VPN Status:</td><td class='mono'>$isp</td></tr>\n";
+    print "         <tr class='w3-hover-black'><td class='header'>Proxy Type:</td><td class='mono'>$usageType</td></tr>\n";
     print "     </table>\n";
     print "     </br>\n";
     print "   </div>\n";
