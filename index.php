e7e5bb31726a7b87f61962494dfe38232969e352
WebBreacher <WebBreacher@users.noreply.github.com>
2021-04-01 11:17:28 -0400

    removing php ip2location reference

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <?php
-    require_once 'IP2Location.php';
-
     #$ip=$_SERVER['REMOTE_ADDR']; // Not used when hosted through CloudFlare/Proxy
     $fwd=$_SERVER['HTTP_X_FORWARDED_FOR'];
     $agent=htmlspecialchars($_SERVER['HTTP_USER_AGENT']);
     $refer=htmlspecialchars($_SERVER['HTTP_REFERER']);
 
     print "<html>\n";
-
     print "<head>\n";
     print "    <meta name='viewport' content='width=device-width, initial-scale=1'>\n";
     print "    <link rel='stylesheet' href='w3.css'>\n";
@@ -21,14 +18,12 @@
     print "</head>\n";
 
     print "<body>\n";
-
     print "<div class='w3-container'>\n";
     print "    <div class='w3-panel w3-card-2 w3-metro-dark-blue'>\n";
     print "        <h2>This is how your browser appears to other sites.</h2>\n";
     print "        <p>This page echoes back to you several pieces of data that web sites 'know' about you. It is meant as a situational awareness tool for you to see how your device presents itself to other sites. To see even more data about your or another IP address, try running the IP2Location Demo <a href='https://www.ip2location.com/demo' target='_blank'>by clicking here</a>.</p>\n";
     print "    </div>\n";
     print "</div>\n";
-
     print "  <div class='center w3-panel w3-card-2 w3-round-xlarge'>\n";
     print "     <table class='w3-table-all'>\n";
     print "     <h4>General Data:</h4>\n";
